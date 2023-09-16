783a41bb9848a189e4eababebadd5604e50cf227
Mr Sharafdin <isasharafdin@gmail.com>
2023-09-16 21:36:13 +0300

    implemented by meta

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <h1 className="text-4xl text-pink-600 flex items-center justify-center">
+      About Muraajaco App
+    </h1>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-4xl text-pink-600 flex items-center justify-center">Welcome to Muraajaco web application</h1>
-  )
+    <h1 className="text-4xl text-pink-600 flex items-center justify-center">
+      Welcome to Muraajaco landing page
+    </h1>
+  );
 }
