98ebe722e3d733f046d926b2f446ad192a7f7b93
Mr Sharafdin <isasharafdin@gmail.com>
2023-09-17 03:17:05 +0300

    implemented by meta

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+  description:
+    "Muraajaco waa mobile app kaas oo loogu talagalay in ardeyda f4 ee soomaliya ka faideystaan.",
+};
+
 export default function About() {
   return (
     <h1 className="text-4xl text-pink-600 flex items-center justify-center">
