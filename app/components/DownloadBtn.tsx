dab40c78c55cff857d3e66111bfa65745152a625
abdinasir-Tman <abdinasirmursal2@gmail.com>
2023-09-24 10:40:21 -0700

    modified

--- app/components/DownloadBtn.tsx
+++ app/components/DownloadBtn.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 const DownloadBtn = () => {
   return (
-    <button className="bg-[#10b981] py-3 flex justify-center items-center px-4 rounded-3xl fixed bottom-5 right-5 text-white font-bold text-lg">
+    <button className="bg-[#10b981] py-3 flex justify-center items-center px-4  rounded-3xl fixed bottom-5 right-5 text-white font-bold text-lg">
       <Image
         alt="murajaco-icon-dark"
         width={40}
         height={40}
         src="/assets/svg/logo/icon-dark.svg"
       />
-      Download now
+      <span className="mr-2">Download now</span>
     </button>
   );
 };
