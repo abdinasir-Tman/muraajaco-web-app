01db7c1f94377b1e79ec9550ea7c288ffc58d232
Omar-jabra <129175570+Omar-Tood@users.noreply.github.com>
2023-12-31 14:57:43 +0300

    completed hero design

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,19 +6,22 @@ const Header = () => {
   return (
     <header className="bg-[#10b981]  py-2 px-2 md:px-48 flex justify-between w-full items-center">
       <Link href="/">
-        {/* <Image
+        <Image
           className="text-[#1A263D]"
-          src="/assets/svg/logo/icon-dark.svg"
+          src="/assets/svg/logo/icon-white.svg"
           alt="Muraajaco-logo-icon-dark"
-          width={80}
-          height={80}
-        /> */}
+          width={70}
+          height={70}
+        />
       </Link>
       <div className="flex space-x-2">
-        <ModeToggle />
+        <div className="fixed z-30 bottom-5 left-5">
+            <ModeToggle />
+        </div>
+        
         <Link
           href="/about"
-          className="text-[#1A263D] active:text-[#10b981] text-2xl"
+          className="text-[#ffffff] active:text-[#10b981] text-xl"
         >
           About
         </Link>
